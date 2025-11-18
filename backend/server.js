e6fbbff73c4ffcfee2517fff18584724a3c659dc
Krishil Shah <KrishilS@stridely.local>
2025-11-18 10:28:48 +0530

    Fix MongoDB connection error: Remove deprecated buffer options
    - Remove bufferCommands and bufferMaxEntries options (deprecated in newer Mongoose)
    - Keep essential connection options: maxPoolSize, serverSelectionTimeoutMS, socketTimeoutMS
    - This fixes the 'option buffermaxentries is not supported' error during deployment

--- backend/server.js
+++ backend/server.js
@@ -104,9 +104,7 @@ mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
   maxPoolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  bufferCommands: false, // Disable mongoose buffering
-  bufferMaxEntries: 0 // Disable mongoose buffering
+  socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
 })
 .then(() => {
   console.log('MongoDB connected successfully');
